8bed7fb0689381f91194b58199ebafa91fb044c0
Dean Silfen <dean.silfen@gmail.com>
2018-11-30 16:27:53 -0600

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-/usr/local/rvm/gems/ruby-2.2.1/bin/middleman b
-rm -rf /var/www/pizza
-mkdir /var/www/pizza
-cp -R build/* /var/www/pizza
-
+/usr/local/bin/middleman b
+rm -rf /srv/build
+cp -R build /srv
